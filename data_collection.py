7b19f5a948f08eda36db92a232224443e8821993
samriddhi448 <samriddhij06@gmail.com>
2021-05-22 13:14:37 +0530

    uploaded scrapper and run code

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 22:18:32 2021
+
+@author: samriddhi
+"""
+import glassdoor_scrapper as gs 
+import pandas as pd 
+
+path = "C:/Users/samriddhi/Desktop/ds_salary_project/chromedriver"
+
+df = gs.get_jobs('data scientist',15, False, path, 15)
+
